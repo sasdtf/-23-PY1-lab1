75ed4f9c55b53530d26f7aa6f0f4c08dbc596dc7
sasdtf <113700571+sasdtf@users.noreply.github.com>
2023-10-15 21:03:10 +0400

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,5 +1,11 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
-disk=1.44*1024*1024
-book=100*50*25*4
-quantity=disk/book
-print("Количество книг, помещающихся на дискету:", int(quantity))
+disk_capacity_bytes = 1.44 * 1024 * 1024
+
+pages = 100
+lines = 50
+chars = 25
+byte_char = 4
+
+book_data_bytes = pages*lines*chars*byte_char
+
+books_disk = disk_capacity_bytes // book_data_bytes
+print("Количество книг, помещающихся на дискету:", int(books_disk))
