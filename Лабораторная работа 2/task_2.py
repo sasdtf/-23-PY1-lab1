6056552aff6475951d4b3983250d4f6dd54de7cb
sasdtf <113700571+sasdtf@users.noreply.github.com>
2023-10-21 16:43:27 +0400

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -2,12 +2,12 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-money_capital = 0  # Начальный баланс подушки безопасности
+money_capital = 0  
 
 for month in range(months):
-    total_spend = spend * ((1 + increase) ** month)  # Рассчитываем расходы для текущего месяца
+    total_spend = spend * ((1 + increase) ** month)  
     deficit = total_spend - salary
-    money_capital += deficit  # Добавляем недостающие средства к подушке безопасности
+    money_capital += deficit  
 
 money_capital=round(money_capital, 2)
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
