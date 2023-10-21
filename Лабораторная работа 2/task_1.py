8cca8be28cc7028cf4b5d41f27adf196cc07e055
sasdtf <113700571+sasdtf@users.noreply.github.com>
2023-10-21 16:41:51 +0400

    Update task_1.py

--- Лабораторная работа 2/task_1.py
+++ Лабораторная работа 2/task_1.py
@@ -2,9 +2,8 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 month=0
+
 while True:
     money_capital=salary+money_capital
     if money_capital>spend:
@@ -13,4 +12,5 @@ while True:
     else:
         break
     spend=spend*(increase+1)
+    
 print("Количество месяцев, которое можно протянуть без долгов:", month)
